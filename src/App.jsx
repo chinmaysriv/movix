211b4480cfe2fa92fdfb5a98708fbc6101796b10
CHINMAY CHITRANSH <chinmay@CHINMAYs-MacBook-Pro.local>
2023-08-26 00:37:02 +0530

    movix app

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {fetchDataFromApi} from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration , getGenres } from "./store/homeSlice";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
+
+
+function App() {
+  const dispatch = useDispatch()
+  const {url} = useSelector((state) => state.home);
+  useEffect(() => {
+    fetchApiConfig();
+    genresCall();
+    }, []);
+
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration")
+     .then((res) => {
+      console.log(res);
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      }
+      dispatch(getApiConfiguration(url));
+     });
+  };
+
+  const genresCall = async () => {
+    let pormises = []
+    let endPoints = ["movie" , "tv"]
+    let allGenres = {}
+    endPoints.forEach((url) => {
+      pormises.push(fetchDataFromApi(`/genre/${url}/list`))
+    });
+    const data = await Promise.all(pormises)
+    data.map(({genres}) => {
+      return genres.map((item) => (allGenres[item.id] = item));
+    });
+    dispatch(getGenres(allGenres));
+  }
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/:mediaType/:id" element={<Details/>} />
+        <Route path="/search/:query" element={<SearchResult/>} />
+        <Route path="/explore/:mediaType" element={<Explore/>} />
+        <Route path="*" element={<PageNotFound/>} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import "./switchtabs.scss";
+
+
+const SwitchTabs = ({data, onTabChange}) => {
+    const [selectedTab, setselectedTab] = useState(0);
+    const [left, setleft] = useState(0);
+    const activeTab = (tab, index) =>{
+        setleft(index * 100);
+        setTimeout(() => {
+            setselectedTab(index);
+        }, 300);
+        onTabChange(tab, index)
+    }
+  return (
+    <div className="switchingTabs">
+        <div className="tabItems">
+            {data.map((tab, index) =>(
+                <span key={index} 
+                className={`tabItem ${selectedTab === index ? "active" : ""}`}
+                onClick={()=>{activeTab(tab, index)}}
+                >
+                    {tab}
+                </span>
+            ))}
+            <span className="movingBg" style={{left}} />
+        </div>
+    </div>
+  )
+}
+
+export default SwitchTabs
